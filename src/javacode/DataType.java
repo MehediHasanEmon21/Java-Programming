aa37a0f1f77451dfd0161ab09d4c8910e4bf95fa
MehediHasanEmon21 <mh.emon2021@gmail.com>
2020-11-18 08:48:50 +0600

    datatype and variable

---
+++ src/javacode/DataType.java
@@ -0,0 +1,21 @@
+
+package javacode;
+
+
+public class DataType {
+
+    
+    public static void main(String[] args) {
+        boolean b;
+        int i;
+        char c;
+        float f;
+        double d;
+        
+        b = true;
+        
+        System.out.println("b = "+b);
+        
+    }
+    
+}
