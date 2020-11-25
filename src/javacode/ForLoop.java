7e6ceb7d8de81815149643e9a8bf5385909be8fa
MehediHasanEmon21 <mh.emon2021@gmail.com>
2020-11-25 12:23:14 -0800

    for loop example

---
+++ src/javacode/ForLoop.java
@@ -0,0 +1,21 @@
+
+package javacode;
+
+import java.util.Scanner;
+
+
+public class ForLoop {
+
+    
+    public static void main(String[] args) {
+        
+      
+            for(int i = 2; i<=100; i= i + 2){
+                
+                System.out.println(i);
+            }
+        
+        
+    }
+    
+}
