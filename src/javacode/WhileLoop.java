82202275c8032ba23f88ccaa96369bdac5d575db
MehediHasanEmon21 <mh.emon2021@gmail.com>
2020-11-25 12:29:03 -0800

    while loop example

--- src/javacode/WhileLoop.java
+++ src/javacode/WhileLoop.java
@@ -4,15 +4,18 @@ package javacode;
 import java.util.Scanner;
 
 
-public class ForLoop {
+public class WhileLoop {
 
     
     public static void main(String[] args) {
         
-      
-            for(int i = 2; i<=100; i= i + 2){
+            int i = 2;
+            while(i <= 100){
                 
                 System.out.println(i);
+                
+                i = i+2;
+                
             }
         
         
