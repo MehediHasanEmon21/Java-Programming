1d17ae6f4959d29228aba742587836dd8842065e
MehediHasanEmon21 <mh.emon2021@gmail.com>
2020-11-28 10:21:53 -0800

    timetable demo

--- src/javacode/TimeTableDemo.java
+++ src/javacode/TimeTableDemo.java
@@ -4,32 +4,37 @@ package javacode;
 import java.util.Scanner;
 
 
-public class LoopDemo {
+public class TimeTableDemo {
 
     
     public static void main(String[] args) {
             
             Scanner input = new Scanner(System.in);
-            int sum = 0;
             int m,n;
-            
-            System.out.print("Enter First number: ");
+           
+            System.out.print("Enter initial number: ");
             m = input.nextInt();
             
-            System.out.print("Enter Second number: ");
+            System.out.print("Enter final number: ");
             n = input.nextInt();
+            
+  
             
             
             for (int i = m; i <= n; i++) {
                 
-                if(i % 2 == 0){
-                    
-                    System.out.print(i+", ");
-                    sum = sum + i;
+                for(int j = 1; j <= 10; j++){
+                    System.out.println(i+" x "+j+ " = "+ i*j);
                 }
+                System.out.println();
+                
+              
+ 
+                   
+                
                 
             }
-            System.out.println("Sum is: "+sum);
+            
         
         
     }
