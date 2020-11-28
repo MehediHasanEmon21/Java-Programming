bde60c5f3367e77621eb5c70c46600c9e25bc524
MehediHasanEmon21 <mh.emon2021@gmail.com>
2020-11-28 11:10:00 -0800

    prime calculation

--- src/javacode/TimeTableDemo.java
+++ src/javacode/TimeTableDemo.java
@@ -10,32 +10,34 @@ public class TimeTableDemo {
     public static void main(String[] args) {
             
             Scanner input = new Scanner(System.in);
-            int m;
-            System.out.print("Enter Any Positive Integer: ");
-            m = input.nextInt();
+            
             int count = 0;
             
             
   
             
             
-            for (int i = 2; i < m; i++) {
+            for (int i = 2; i <= 100; i++) {
                 
-                if(m % i == 0){
+                
+                for (int j = 2; j <= i-1; j++) {
                     
-                    count++;
-                    break;
+                    if(i % j == 0){
+                        
+                        count++;
+                        break;
+                        
+                    }
                     
                 }
+                if(count == 0)
+                System.out.println(i);
+                count = 0;
                 
                 
             }
             
-            if(count == 0){
-                System.out.println("Prime Number");
-            }else{
-                System.out.println("Not a Prime Number");
-            }
+            
             
         
         
