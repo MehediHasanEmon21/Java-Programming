edef3c426a042d1b42c09bd39959bc07ffd1a9e5
MehediHasanEmon21 <mh.emon2021@gmail.com>
2020-12-09 15:14:00 -0800

    pattern 1

---
+++ src/javacode/Pattern1.java
@@ -0,0 +1,48 @@
+
+package javacode;
+
+import java.util.Scanner;
+
+
+public class Pattern1 {
+
+    
+    public static void main(String[] args) {
+            
+            Scanner input = new Scanner(System.in);
+            
+            int n;
+            System.out.print("Enter How Many row: ");
+            n = input.nextInt();
+            
+            for(int row = 1; row<=n; row++){
+                
+                for(int col = 1; col<=row; col++){
+                    
+                    System.out.print(" "+col);
+                    
+                }
+                System.out.println("");
+                
+            }
+            
+            
+            
+            
+            
+            
+  
+            
+            
+           
+                
+                
+          
+            
+            
+            
+        
+        
+    }
+    
+}
