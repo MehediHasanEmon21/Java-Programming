6003bba9d92c646722b05d998d2e981e4580719d
MehediHasanEmon21 <mh.emon2021@gmail.com>
2020-12-13 02:22:25 -0800

    introduction of array

---
+++ src/javacode/Array.java
@@ -0,0 +1,50 @@
+
+package javacode;
+
+import java.util.Scanner;
+
+
+public class Array {
+
+    
+    public static void main(String[] args) {
+            
+        int[] number = new int[10];
+        
+        number[0] = 10;
+        number[1] = 10;
+        number[2] = 10;
+        number[3] = 10;
+        number[4] = 10;
+        int sum = 0;
+        
+        for(int i = 0; i < number.length; i++){
+            sum = sum + number[i];
+        }
+        
+        System.out.println("Sum is :"+sum);
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+  
+            
+            
+           
+                
+                
+          
+            
+            
+            
+        
+        
+    }
+    
+}
